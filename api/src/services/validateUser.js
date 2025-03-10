590e052b2bbd56ff081646f602484b6815e99697
Malu1313 <marialuisavinholireis@gmail.com>
2025-03-10 16:20:15 -0300

    10_03

--- api/src/services/validateUser.js
+++ api/src/services/validateUser.js
@@ -8,11 +8,17 @@ module.exports = function validateUser({
   if (!cpf || !email || !password || !name || !data_nascimento) {
     return { error: "Todos os campos devem ser preenchidos" };
   }
-  if(isNaN(cpf) ||cpf.length !==11){
-    return {error:"CPF inválido, deve conter 11 dígitos numéricos"}
-  } 
+
+  if (isNaN(cpf) || cpf.length !== 11) {
+    return {
+      error: "CPF inválido. Deve conter exatamente 11 dígitos numéricos",
+    };
+  }
+
   if (!email.includes("@")) {
     return { error: "Email inválido. Deve conter @" };
   }
-  return null; //Ou seja, se estiver tudo certo eu retorno nulo para ignorar o IF na userController
+
+  return null; // Retorna null se não houver erro
 };
+
